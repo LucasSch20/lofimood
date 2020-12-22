c59351036a6d7afa1230e718dd4938198fd49e2a
Lucas L. Schimidt <70489851+LucasSch20@users.noreply.github.com>
2020-12-22 20:08:07 -0300

    v0.0.3 (Alpha)

--- README.md
+++ README.md
@@ -2,9 +2,12 @@
  A Lofi hiphop site.
 
 ## A Fazer:
-* Terminar de desenvolver index do site []
+* Terminar de desenvolver index do site [X]
 * Criar o sistema do player []
 * Criar o sistema das imagens e gifs []
 * Fazer a página do perfil []
 * Fazer a página das Config []
 * Fazer a página das Imagens e GIFS []
+
+## Bugs e problemas:
+* As vezes a animação do player subindo buga, quando o mouse passa muito rápido; Corrigir []
--- js/player-updown.js
+++ js/player-updown.js
@@ -2,11 +2,16 @@ let player = document.querySelector('.player')
 let noplayer = document.querySelector('.no-player')
 let mainframe = document.querySelector('.main-frame')
 
-noplayer.addEventListener('mouseenter', function() {
-    player.style.display = 'block';
+noplayer.addEventListener('mouseenter', function () {
+    player.style.animationName = 'playerUp';
+    player.style.animationDuration = '0.5s';
+    player.style.animationFillMode = 'forwards';
     noplayer.style.display = 'none';
 })
-mainframe.addEventListener('mouseover', function() {
+
+player.addEventListener('mouseleave', function () {
+    player.style.animationName = 'playerDown';
+    player.style.animationDuration = '0.5s';
     noplayer.style.display = 'block';
-    player.style.display = 'none';
 })
+
