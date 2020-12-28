dff6a174013cd9bccbd757833e88fc7f881d060c
Lucas L. Schimidt <70489851+LucasSch20@users.noreply.github.com>
2020-12-28 17:57:04 -0300

    v0.0.8(Alpha)

--- js/Agifs/gif-system.js
+++ js/Agifs/gif-system.js
@@ -1,17 +1,31 @@
 var frame = document.querySelector('.main-frame');
-var currentGIF = Math.floor(Math.random() * (2 - 0 + 1)) + 0;
-if (agora == 'day'){
+var currentGIF = Math.floor(Math.random() * (5 - 0 + 1)) + 0;
+if (agora == 'day') {
     frame.style.backgroundImage = gifListDay[currentGIF].src;
 } else {
     frame.style.backgroundImage = gifListNight[currentGIF].src;
 }
 
-window.setInterval(function(){
-    currentGIF =  currentGIF + 1;
+window.setInterval(function () {
 
-    if (agora == 'day'){
+    currentGIF = currentGIF + 1;
+
+    maxGif();
+
+    if (agora == 'day') {
         frame.style.backgroundImage = gifListDay[currentGIF].src;
     } else {
         frame.style.backgroundImage = gifListNight[currentGIF].src;
     }
-}, 180000);
+}, 60000);
+
+function maxGif() {
+    if (currentGIF > 5) {
+        currentGIF = 0;
+        if (agora == 'day') {
+            frame.style.backgroundImage = gifListDay[currentGIF].src;
+        } else {
+            frame.style.backgroundImage = gifListNight[currentGIF].src;
+        }
+    }
+}
--- js/Agifs/gifList.js
+++ js/Agifs/gifList.js
@@ -7,7 +7,19 @@ var gifListDay = [
     },
     {
         src: "url('../../itens/images/day/3.gif')"
-    }
+    },
+    {
+        src: "url('../../itens/images/day/4.gif')"
+    },
+    {
+        src: "url('../../itens/images/day/5.gif')"
+    },
+    {
+        src: "url('../../itens/images/day/6.gif')"
+    },
+    {
+        src: "url('../../itens/images/day/7.gif')"
+    },
 ];
 
 var gifListNight = [
@@ -22,5 +34,17 @@ var gifListNight = [
     },
     {
         src: "url('../../itens/images/night/4.gif')"
-    }
+    },
+    {
+        src: "url('../../itens/images/night/5.gif')"
+    },
+    {
+        src: "url('../../itens/images/night/6.gif')"
+    },
+    {
+        src: "url('../../itens/images/night/7.gif')"
+    },
+    {
+        src: "url('../../itens/images/night/8.gif')"
+    },
 ];
