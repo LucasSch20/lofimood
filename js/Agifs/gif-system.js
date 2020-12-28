067b88947098309cbe4b9368f02c0a7189c75dea
Lucas L. Schimidt <70489851+LucasSch20@users.noreply.github.com>
2020-12-28 10:49:26 -0300

    v0.0.8(Alpha)

--- js/Agifs/gif-system.js
+++ js/Agifs/gif-system.js
@@ -1,9 +1,9 @@
 var frame = document.querySelector('.main-frame');
-var currentGIF = 0;
+var currentGIF = Math.floor(Math.random() * (2 - 0 + 1)) + 0;
 if (agora == 'day'){
-    frame.style.backgroundImage = gifListDay[0].src;
+    frame.style.backgroundImage = gifListDay[currentGIF].src;
 } else {
-    frame.style.backgroundImage = gifListNight[0].src;
+    frame.style.backgroundImage = gifListNight[currentGIF].src;
 }
 
 window.setInterval(function(){
--- js/player/player-funcionalities-base.js
+++ js/player/player-funcionalities-base.js
@@ -3,15 +3,20 @@ var play = document.querySelector('#play-btn');
 var pause = document.querySelector('#pause-btn');
 var music = document.querySelector('#audio')
 var trackName = document.querySelector('#tn-text');
-var trackNum = 0;
+//var TLlenght = Number(trackList.length);
+
+var trackNum = Math.floor(Math.random() * (3 - 0 + 1)) + 0;
 var credits = document.querySelector('#credit');
 var creditsLink = document.querySelector('#credit-link');
 
 //Créditos da Música
 
 window.setTimeout(() => {
-    credits.innerHTML = trackList[0].credits;
-creditsLink.innerHTML = trackList[0].creditsLink;
+    music.src = trackList[trackNum].src;
+    trackName.innerHTML = trackList[trackNum].name;
+
+    credits.innerHTML = trackList[trackNum].credits;
+    creditsLink.innerHTML = trackList[trackNum].creditsLink;
 }, 1);
 
 
